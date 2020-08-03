c7710054705ea3c7361b66c9ff949ca4db6fef0c
codedarkness <achim.maradiaga@gmail.com>
2020-08-02 21:55:41 -0700

    Added new options

--- tmux-install.sh
+++ tmux-install.sh
@@ -21,32 +21,48 @@
 #
 # -----------------------------------------------------------------
 
-arch-based() {
-	echo "";
-	echo " Installing tmux - ArchBased systems"
-	sleep 2;
-	pacman -Qs tmux &&
-		echo " tmux is installed in your system" ||
-		sudo pacman -S --noconfirm -needed tmux
-}
+install-tmux() {
+	echo ""
+	echo " Install tmux in Arch, Debian based systems"
+	echo ""
+	sleep 2
 
-debian-based() {
-	echo "";
-	echo " Installing tmux - DebianBased systems"
-	sleep 2;
-	dpkg -l | grep tmux &&
-		echo " tmux is installed in your system" ||
-		sudo apt install -y tmux
+
+	if ! location="$(type -p "tmux")" || [ -z "tmux" ]; then
+
+		# check if pacman is installed
+		if which pacman > /dev/null; then
+
+			sudo pacman -S --noconfirm --needed tmux
+
+		fi
+
+		# check if apt is installed
+		if which apt > /dev/null; then
+
+			sudo apt install -y tmux
+
+		fi
+
+	else
+		echo " nothing to do!"
+	fi
 }
 
 config-files() {
 	echo ""
-	echo " Copy custom files";
-	cp -ar config-files/tmux.conf $HOME/.tmux.conf &&
-		echo " tmux.conf was copied";
-	cp -ar config-files/tmux-dev.sh $HOME/tmux-dev.sh &&
-		echo " custom tmux-dev was copied";
+	echo " Copy custom files"
 	echo ""
+	sleep 2
+
+	cp -ar config-files/configs/tmux.conf $HOME/.tmux.conf &&
+	echo " tmux.conf was copied" || echo " Upssss!!!"
+	echo ""
+
+	cp -ar config-files/configs/tmux-dev.sh $HOME/tmux-dev.sh &&
+	echo " custom tmux-dev was copied" || echo " It's better if you reformat your pc!!"
+	echo ""
+
 	echo " in your terminal run from home directory ./tmux-dev.sh"
 }
 
@@ -75,14 +91,13 @@ until [ "$selection" = "0" ]; do
 	echo " | __| '_ ' _ \| | | \ \/ / "
 	echo " | |_| | | | | | |_| |>  <  "
 	echo "  \__|_| |_| |_|\__,_/_/\_\ "
-	echo " ------------------------------"
-	echo " ### a terminal multiplexer ###"
-	echo " ------------------------------"
 	echo ""
-	echo " 1 - Install in Arch Based"
-	echo " 2 - Install in Debian Based"
-	echo " 3 - Copy (custom) config files"
-	echo " 4 - Keybindings"
+	echo " a terminal multiplexer"
+	echo ""
+	echo " 1 - Install tmux"
+	echo " 2 - Copy (custom) config files"
+	echo " 3 - Keybindings"
+	echo ""
 	echo " 0 - Exit"
 	echo ""
 	echo -n " Enter selection [1 - 0] : "
@@ -90,10 +105,9 @@ until [ "$selection" = "0" ]; do
 	echo ""
 
 	case $selection in
-		1) clear; arch-based   ; press_enter ;;
-		2) clear; debian-based ; press_enter ;;
-		3) clear; config-files ; press_enter ;;
-		4) clear; keybindings ;;
+		1) clear; install-tmux  ; press_enter ;;
+		2) clear; config-files  ; press_enter ;;
+		3) clear; keybindings  ;;
 		0) clear; exit ;;
 		*) clear; incorrect_selection ; press_enter ;;
 	esac
