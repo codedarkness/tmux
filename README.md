c65dabe752050637568a3597a6e65cc0f63a8cab
codedarkness <achim.maradiaga@gmail.com>
2023-11-01 19:13:26 -0700

    update scripts, added new config files

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # Tmux
 
-tmux is a terminal multiplexer: it enables a number of terminals to be created, accessed, and controlled from a single screen. tmux may be detached from a screen and continue running in the background, then later reattached. Learn more about [tmux](https://github.com/tmux/tmux).
+tmux is a terminal multiplexer: 
+
+* it enables a number of terminals to be created, accessed, and controlled from a single screen.
+* tmux may be detached from a screen and continue running in the background, then later reattached.
+* Learn more about [tmux](https://github.com/tmux/tmux).
 
 ![tmux - darknesscode](/config-files/tmux.png)
 
-Prefix key = C-a
+Prefix key = C-u
+
+Source config file = C-u + r
 
 Mouse support is on.
 
-| Keys		         | Action		               |
+| Keys             | Action	                 |
 | :-------------   | :---------------------- |
 | \<C-a\> + \|     | Split Window Horizontal |
 | \<C-a\> + -      | Split Window Vertical   |
@@ -16,9 +22,8 @@ Mouse support is on.
 | \<Alt\> + Arrows | Switch Panels           |
 | \<C-a\> + 0-1    | Switch windows          |
 | \<C-a\> + w      | Shows windows list      |
-| \<C-a\> + r      | Reload config           |
 | \<C-a\> + c      | Create new window       |
 | \<C-a\> + ,      | Change name of window   |
-| \<C-a\> + x   	 | Kill window (panel)	   |
+| \<C-a\> + x      | Kill window (panel)	 |
 
 For more kybindings [tmux cheat sheet](https://tmuxcheatsheet.com/)
--- tmux-install.sh
+++ tmux-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #  ____             _                         ____          _
 # |  _ \  __ _ _ __| | ___ __   ___  ___ ___ / ___|___   __| | ___
 # | | | |/ _' | '__| |/ / '_ \ / _ \/ __/ __| |   / _ \ / _' |/ _ \
@@ -21,41 +21,110 @@
 #
 # -----------------------------------------------------------------
 
+_user="$USER"
+_os="$(cat /etc/os-release | grep PRETTY_NAME | cut -d = -f 2 | tr -d '"')"
+
+## Colors
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+END="\e[0m"
+
 install-tmux() {
-	echo ""
-	echo " Install tmux"
-	echo " Arch Linux | Debian | Void Linux"
+	echo -e "Installing ${RED}tmux${END} multiplexer"
+	echo -e "${YELLOW}$_os${END}"
 	echo ""
 	sleep 2
 
 	while true; do
-		read -p " Install tmux [y - n] : " yn
+		read -p "Install tmux [y - n] : " yn
 		case $yn in
 			[Yy]* )
 				if ! location="$(type -p "tmux")" || [ -z "tmux" ]; then
-
 					# check if pacman is installed
 					if which pacman > /dev/null 2>&1; then
-
 						sudo pacman -S --noconfirm tmux
-
 					# check if apt is installed
 					elif which apt > /dev/null 2>&1; then
-
 						sudo apt install -y tmux
-
 					# check if xbps is installed
 					elif which xbps-install > /dev/null 2>&1; then
-
 						sudo xbps-install -Sy tmux
 					else
-
-						echo " Your system is not compatible with this script..."
+						echo -e "${RED}Your system is not compatible with this script...${END}"
 					fi
-
 					else
-						echo " Nothing to do! tmux is installed in your System"
+						echo -e "Nothing to do! ${GREEN}tmux is installed${END} in your System"
 				fi ; break ;;
+			[Nn]* )
+				break ;;
+			* ) echo "Please answer yes or no." ;;
+		esac
+	done
+}
+
+uninstall-tmux() {
+	echo -e "Un-Installing ${RED}tmux${END} multiplexer"
+	echo -e "${YELLOW}$_os${END}"
+	echo ""
+	sleep 2
+
+	while true; do
+		read -p "unistall tmux [y - n] : " yn
+		case $yn in
+			[Yy]* )
+				if location="$(type -p "tmux")" || [ -z "tmux" ]; then
+					# check if pacman is installed
+					if which pacman > /dev/null 2>&1; then
+						sudo pacman -Rs --noconfirm tmux
+					# check if apt is installed
+					elif which apt > /dev/null 2>&1; then
+						sudo apt remove --purge -y tmux
+					# check if xbps is installed
+					elif which xbps-install > /dev/null 2>&1; then
+						sudo xbps-remove -f tmux
+					else
+						echo -e "${RED}Your system is not compatible with this script...${END}"
+					fi
+					else
+						echo -e "Nothing to do! ${RED}tmux is not installed${END} in your System"
+				fi ; break ;;
+			[Nn]* )
+				break ;;
+			* ) echo "Please answer yes or no." ;;
+		esac
+	done
+}
+
+config-files() {
+	echo -e "${BLUE}Copy Custom Files${END}"
+	echo ""
+	sleep 2
+
+	[ ! -d "$HOME/.config/tmux" ] && mkdir -p $HOME/.config/tmux && echo -e "${GREEN}tmux directory in .config${END} Created" || echo -e "${RED}LinuxSucks${END}....."
+
+	declare -A directories
+	directories[tmux-sys]="config-files/tmux-sys $HOME/.config"
+	directories[tmux-temp]="config-files/tmux-temp $HOME/.config"
+
+	for directory in "${directories[@]}"; do
+		cp -r $directory && echo -e "${GREEN}$directory${END} Copied" || echo -e "${RED}LinuxSucks${END}....."
+	done
+
+	[ ! -f "$HOME/.tmux.conf" ] && cp -ar config-files/configs/tmux.conf $HOME/.tmux.conf && echo -e "${GREEN}tmux.conf${END} Capied" || echo -e "${RED}LinuxSucks${END}....."
+}
+
+edit-files() {
+	echo -e "${BLUE}Edit tmux.conf file${END}"
+	echo ""
+	sleep 1
+
+	while true; do
+		read -p "Edit in home .tmux.conf [y - n] : " yn
+		case $yn in
+			[Yy]* )
+				[ -f "$HOME/.tmux.conf" ] && vim $HOME/.tmux.conf || echo -e "${RED}LinuxSucks${END}....." ; break;;
 			[Nn]* )
 				break ;;
 			* ) echo "Please answer yes or no." ;;
@@ -63,17 +132,17 @@ install-tmux() {
 	done
 
 	echo ""
-}
 
-config-files() {
-	echo ""
-	echo " Copy custom files"
-	echo ""
-	sleep 2
-
-	cp -ar config-files/configs/tmux.conf $HOME/.tmux.conf &&
-	echo " tmux.conf was copied" || echo " Upssss!!!"
-	echo ""
+	while true; do
+		read -p "Edit config-files tmux.conf [y - n] : " yn
+		case $yn in
+			[Yy]* )
+				[ -f "config-files/configs/tmux.conf" ] && vim config-files/configs/tmux.conf || echo -e "${RED}LinuxSucks${END}....." ; break;;
+			[Nn]* )
+				break ;;
+			* ) echo "Please answer yes or no." ;;
+		esac
+	done
 }
 
 keybindings() {
@@ -82,42 +151,51 @@ keybindings() {
 
 press_enter() {
 	echo ""
-	echo -n " Press Enter To Continue"
+	echo -n "Press Enter To Continue"
 	read
 	clear
 }
 
 incorrect_selection() {
-	echo " Incorrect selection! try again"
+	echo -e "${RED}Incorrect selection! try again${END}"
 }
 
 until [ "$selection" = "0" ]; do
 	clear
+	echo -e "${BLUE}DarknessCode${END}"
+	echo " _                         "
+	echo "| |                        "
+	echo "| |_ _ __ ___  _   ___  __ "
+	echo "| __| '_ ' _ \| | | \ \/ / "
+	echo "| |_| | | | | | |_| |>  <  "
+	echo " \__|_| |_| |_|\__,_/_/\_\ "
+	echo -e "${GREEN}A terminal multiplexer${END}"
 	echo ""
-	echo " DarknessCode"
-	echo "  _                         "
-	echo " | |                        "
-	echo " | |_ _ __ ___  _   ___  __ "
-	echo " | __| '_ ' _ \| | | \ \/ / "
-	echo " | |_| | | | | | |_| |>  <  "
-	echo "  \__|_| |_| |_|\__,_/_/\_\ "
+	echo -e "Welcome: ${RED}$_user${END}"
 	echo ""
-	echo " a terminal multiplexer"
+	echo -e "${YELLOW}Installation${END}"
+	echo "-------------------------------"
+	echo -e "${GREEN}[1]${END} - Install tmux"
+	echo -e "${GREEN}[2]${END} - Un-Install tmux"
 	echo ""
-	echo " 1 - Install tmux"
-	echo " 2 - Copy (custom) files"
-	echo " 3 - Keybindings"
+	echo -e "${YELLOW}Edit & Custom files${END}"
+	echo "-------------------------------"
+	echo -e "${GREEN}[3]${END} - Edit tmux.conf file"
+	echo -e "${GREEN}[4]${END} - Copy (custom) files"
+	echo -e "${GREEN}[5]${END} - Keybindings"
 	echo ""
-	echo " 0 - Exit"
+	echo -e "${GREEN}[0]${END} - Exit"
 	echo ""
-	echo -n " Enter selection [1 - 0] : "
+	echo -n "Enter selection [1 - 0] : "
 	read selection
 	echo ""
 
 	case $selection in
 		1) clear; install-tmux  ; press_enter ;;
-		2) clear; config-files  ; press_enter ;;
-		3) clear; keybindings  ;;
+		2) clear; uninstall-tmux  ; press_enter ;;
+		3) clear; edit-files  ; press_enter ;;
+		4) clear; config-files  ; press_enter ;;
+		5) clear; keybindings  ;;
 		0) clear; exit ;;
 		*) clear; incorrect_selection ; press_enter ;;
 	esac
